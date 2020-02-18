e452a77b7bc9c2c024da225784dce8907be60f9a
Tóth Máté <tothmate911@gmail.com>
2020-02-18 12:12:00 +0100

    show boards

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,5 +1,5 @@
 // It uses data_handler.js to visualize elements
-import { dataHandler } from "./data_handler.js";
+import {dataHandler} from "./data_handler.js";
 
 export let dom = {
     init: function () {
@@ -7,7 +7,7 @@ export let dom = {
     },
     loadBoards: function () {
         // retrieves boards and makes showBoards called
-        dataHandler.getBoards(function(boards){
+        dataHandler.getBoards(function (boards) {
             dom.showBoards(boards);
         });
     },
@@ -17,9 +17,14 @@ export let dom = {
 
         let boardList = '';
 
-        for(let board of boards){
+        for (let board of boards) {
             boardList += `
-                <li>${board.title}</li>
+                <section class="board">
+                    <div class="board-header"><span class="board-title">${board.title}</span>
+                    <button class="board-add">Add Card</button>
+                    <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
+                </div>
+                </section>
             `;
         }
 
@@ -29,11 +34,14 @@ export let dom = {
             </ul>
         `;
 
-        let boardsContainer = document.querySelector('#boards');
+        let boardsContainer = document.querySelector('.board-container');
         boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
+        dataHandler.getCardsByBoardId(boardId, function (cards) {
+            dom.showBoards(cards);
+        });
     },
     showCards: function (cards) {
         // shows the cards of a board
