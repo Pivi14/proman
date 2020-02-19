a3eba8f7eeb928952d9315ccad82591200f7ee32
pivarcsi <pivarcsiadam@gmail.com>
2020-02-19 18:49:18 +0100

    default animation

--- static/js/dom.js
+++ static/js/dom.js
@@ -9,6 +9,48 @@ function new_board() {
 }
 
 
+function chevron_down(board_id) {
+    let board_columns = document.getElementById(`board-columns${board_id}`);
+    let elements = board_columns.childNodes;
+    if (elements.length !== 0){
+        board_columns.animate([
+            {height: "100px"},
+            {height: "0px"}
+        ], {
+            duration: 500
+        });
+        setTimeout(function () {
+            for (let element of elements){
+                element.style.display = 'none'
+            }
+        }, 250);
+        document.getElementById(`chevron-image${board_id}`).classList.remove('fa-chevron-down');
+        document.getElementById(`chevron-image${board_id}`).classList.add('fa-chevron-up');
+        document.getElementById(`toggle${board_id}`).removeEventListener('click', chevron_down);
+        document.getElementById(`toggle${board_id}`).addEventListener('click', function () {chevron_up(board_id)})
+    }
+}
+
+function chevron_up(board_id){
+    let board_columns = document.getElementById(`board-columns${board_id}`);
+    let elements = board_columns.childNodes;
+    board_columns.animate([
+        {height: "0px"},
+        {height: '100px'}
+    ],{
+        duration: 500
+    });
+    setTimeout(function () {
+        for (let element of elements){
+            element.style.display = 'block'
+        }
+    }, 250);
+    document.getElementById(`chevron-image${board_id}`).classList.remove('fa-chevron-up');
+    document.getElementById(`chevron-image${board_id}`).classList.add('fa-chevron-down');
+    document.getElementById(`toggle${board_id}`).removeEventListener('click', chevron_up);
+    document.getElementById(`toggle${board_id}`).addEventListener('click', function () {chevron_down(board_id)})
+
+}
 
 function build_board(card) {
     let create_board = document.createElement('section');
@@ -24,8 +66,11 @@ function build_board(card) {
     board_add.innerText = 'Add Card';
     let board_toggle = document.createElement('button');
     board_toggle.setAttribute('class', 'board-toggle');
+    board_toggle.id = `toggle${card.board_id}`;
+    board_toggle.addEventListener('click', function () {chevron_down(card.board_id)});
     let image = document.createElement('i');
     image.setAttribute('class', 'fas fa-chevron-down');
+    image.id = `chevron-image${card.board_id}`;
     let board_columns = document.createElement('div');
     board_columns.id=`board-columns${card.board_id}`;
     board_columns.setAttribute('class', 'board-columns');
