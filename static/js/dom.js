026381855b2e7f11d0ea49b00ab56505f255f6c5
Blaon <iloveelevation@gmail.com>
2020-02-19 11:00:23 +0100

    show table working

--- static/js/dom.js
+++ static/js/dom.js
@@ -19,6 +19,7 @@ function build_board(card) {
     let image = document.createElement('i');
     image.setAttribute('class', 'fas fa-chevron-down');
     let board_columns = document.createElement('div');
+    board_columns.id=`board-columns${card.board_id}`;
     board_columns.setAttribute('class', 'board-columns');
     board_toggle.appendChild(image);
     board_header.appendChild(board_title);
@@ -27,6 +28,23 @@ function build_board(card) {
     create_board.appendChild(board_header);
     create_board.appendChild(board_columns);
     document.getElementById('board-container').appendChild(create_board);
+}
+
+function build_column(card) {
+    let board_column = document.createElement('div');
+    board_column.setAttribute('class', 'board-column');
+    board_column.id = `col${card.col_id}`;
+    let board_column_title = document.createElement('div');
+    board_column_title.setAttribute('class', 'board-column-title');
+    board_column_title.innerText = card.col_title;
+    let board_column_content = document.createElement('div');
+    board_column_content.id = `board-column-content${card.col_id}`;
+    board_column_content.setAttribute('class', 'board-column-content');
+
+    board_column.appendChild(board_column_title);
+    board_column.appendChild(board_column_content);
+
+    document.getElementById(`board-columns${card.board_id}`).appendChild(board_column);
 }
 
 export let dom = {
@@ -45,6 +63,27 @@ export let dom = {
             if (board === null) {
                 build_board(card);
             }
+            let columns=document.getElementById(`col${card.col_id}`);
+            if (columns===null){
+                build_column(card);
+            }
+            let individual_card=document.createElement('div');
+            individual_card.setAttribute('class','card');
+            individual_card.id=`card${card.id}`;
+            let card_remove=document.createElement('div');
+            card_remove.setAttribute('class','card-remove');
+            let image=document.createElement('i');
+            image.setAttribute('class','fas fa-trash-alt');
+            let card_title=document.createElement('div');
+            card_title.setAttribute('class','card-title');
+            card_title.innerText=card.card_title;
+
+            card_remove.appendChild(image);
+            individual_card.appendChild(card_remove);
+            individual_card.appendChild(card_title);
+            document.getElementById(`board-column-content${card.col_id}`).appendChild(individual_card);
+
+
         }
     },
     loadCards: function (boardId) {
